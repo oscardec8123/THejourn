8a979beffaa778d5b2bee053e022862b9946dfd7
oscardec8123 <you@example.com>
2024-11-23 09:42:25 +0100

    this is bash file and was edited

--- DASH.bash
+++ DASH.bash
@@ -2,4 +2,4 @@
 
 echo "the lord is my helper i shall not want, he maketh me to lie down
 in green Pasture, he restoreth my soul for his Names's Sake, Yea though i walk through the 
-valley of shadow of death i shall not die, for his rock and his staff confort me."
+valley of shadow of death i shall not die, for his rock and his staff confort me."aaa:
